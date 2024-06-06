cf90526163343bc63384babc3eb201637d6d0ed2
Norbert Csaba Herczeg <noherczeg@gmail.com>
2024-06-06 03:07:21 +0200

    JNG-5742 extract utils

--- judo-ui-typescript-rest-itest/src/test/resources/tests/serialization.test.ts
+++ judo-ui-typescript-rest-itest/src/test/resources/tests/serialization.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, suite } from 'vitest';
-import { draftIdentifierPrefix } from '../src/services/data-api/common/Serializer';
+import { draftIdentifierPrefix } from '../src/services/data-api/common/utils';
 import type { ViewGalaxyStored } from '../src/services/data-api/model/ViewGalaxy';
 import { ViewGalaxyStoredSerializer } from '../src/services/data-api/rest/ViewGalaxySerializer';
 
@@ -7,28 +7,33 @@ suite('Serialization Tests', () => {
     const galaxySerializer = new ViewGalaxyStoredSerializer();
 
     const serialized = Object.freeze({
+        __identifier: 'id1',
         __signedIdentifier: 'aaa',
-        astronomer: {
+        astronomer: Object.freeze({
+            __identifier: 'id4',
             __signedIdentifier: 'ttt',
             born: '2022-03-25',
             name: 'bello'
-        },
+        }),
         constellation: 'test const',
         magnitude: 1.22,
         nakedEye: true,
         name: 'hello',
         stars: [
-            {
+            Object.freeze({
+                __identifier: 'id2',
                 __signedIdentifier: 'ccc',
                 name: 'yoo',
-            },
-            {
+            }),
+            Object.freeze({
+                __identifier: 'id3',
                 __signedIdentifier: 'ggg',
                 name: 'boo'
-            },
+            }),
         ],
     });
     const galaxy: ViewGalaxyStored = {
+        __identifier: 'id1',
         __signedIdentifier: 'aaa',
         name: 'hello',
         constellation: 'test const',
@@ -36,15 +41,18 @@ suite('Serialization Tests', () => {
         magnitude: 1.22,
         stars: [
             {
+                __identifier: 'id2',
                 __signedIdentifier: 'ccc',
                 name: 'yoo',
             },
             {
+                __identifier: 'id3',
                 __signedIdentifier: 'ggg',
                 name: 'boo',
             },
         ],
         astronomer: {
+            __identifier: 'id4',
             __signedIdentifier: 'ttt',
             name: 'bello',
             born: new Date('2022-03-25'),
@@ -63,7 +71,7 @@ suite('Serialization Tests', () => {
             name: 'hello',
             constellation: 'test const',
         };
-        const result = galaxySerializer.serialize(galaxyToClean, true);
+        const result = galaxySerializer.serialize(galaxyToClean);
         expect(result).toEqual({
             __signedIdentifier: 'bbb',
             name: 'hello',
